73354086367d218a4cc9e0d201145f0f356657e9
Emode Ohimai <67082364+ohis-coder@users.noreply.github.com>
2025-12-27 21:08:22 +0100

    Initial Commit

---
+++ RootSwitch/RootSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RootSwitchApp.swift
+//  RootSwitch
+//
+//  Created by ohis on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct RootSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
